cbd85fc38ced566ec5b1d99f1a4cb722e03243ab
Kimjingon <48813851+Kimjingon@users.noreply.github.com>
2019-06-04 02:37:27 +0900

    Update TabFragment1.java
    checkbox 로 list 삭제 구현 추가

--- ProjectRoot/app/src/main/java/com/example/user/mynavigation/TabFragment1.java
+++ ProjectRoot/app/src/main/java/com/example/user/mynavigation/TabFragment1.java
@@ -9,21 +9,25 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
 
 // This class is for first fragment which shows the list of setted alarms
 public class TabFragment1 extends Fragment{
     CustomChoiceListViewAdapter adapter = new CustomChoiceListViewAdapter();
     //ListView listView = (ListView)getView().findViewById(R.id.listview1);
+
     ListView listView;
+
+
     //listView.setAdapter(adapter);
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.tab_fragment_1, container, false);
-
-
         listView = (ListView)view.findViewById(R.id.listview1);
 
         listView.setAdapter(adapter);
@@ -35,13 +39,41 @@ public class TabFragment1 extends Fragment{
 //        adapter.addItem("Vitamin E");
         adapter.notifyDataSetChanged();
 
+        Button deleteButton = (Button)view.findViewById(R.id.button) ;
+        deleteButton.setOnClickListener(new Button.OnClickListener() {
+            public void onClick(View v) {
+                int count, checked ;
+                count = adapter.getCount() ;
+
+                if (count > 0) {
+                    // 현재 선택된 아이템의 position 획득.
+                    checked = listView.getCheckedItemPosition();
+
+                    if (checked > -1 && checked < count) {
+                        // 아이템 삭제
+                        adapter.RemoveData(checked) ;
+
+                        // listview 선택 초기화.
+                        listView.clearChoices();
+
+                        // listview 갱신.
+                        adapter.notifyDataSetChanged();
+                    }
+                }
+            }
+        }) ;
+
         return view;
+
+
     }
 
-    public void addPlan(String plan, String hour, String minute, boolean[] check_days) {
+
+
+        public void addPlan(String plan, String hour, String minute, boolean[] check_days) {
         // Toast.makeText(getContext(), "add plan 함수 진입", Toast.LENGTH_SHORT);
         // listView.setAdapter(adapter);
-        String result = plan + " " + hour + "시 " + minute + "분\n";
+        String result = "내용 : "+ plan + " \n" +"시간 :"+  hour + "시 " + minute + "분\n"+"요일 : ";
 
         if(check_days[0] == true) result += "월";
         if(check_days[1] == true) result += "화";
@@ -60,4 +92,6 @@ public class TabFragment1 extends Fragment{
         adapter.notifyDataSetChanged();
         //listView.setAdapter(adapter);
     }
+
+
 }
